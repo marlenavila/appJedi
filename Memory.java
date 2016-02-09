5e61f0e87b4c4ce47afdd9133085493127c6541a
marlen <marlen.avila93@gmail.com>
2016-02-09 17:37:31 +0100

    memory half done almost finished gps added to profile user action_pick added to profile user

--- Memory.java
+++ Memory.java
@@ -1,13 +1,205 @@
 package com.example.marlen.appjedi;
 
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.material.joanbarroso.flipper.CoolImageFlipper;
+
+import java.util.Random;
 
 public class Memory extends AppCompatActivity {
+
+    CoolImageFlipper coolImageFlipper;
+    ImageView i0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_memory);
+        setContentView(R.layout.container);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MemoryGameFragment())
+                    .commit();
+        }
+    }
+
+    public static class MemoryGameFragment extends Fragment {
+        private static final int NUM_IMAGES = 16;
+        private static final String LOG_TAG = MemoryGameFragment.class.getSimpleName();
+
+        private boolean busy;
+        private Handler handler = new Handler();
+
+        private ImageView[] images_list = new ImageView[NUM_IMAGES];
+        private int[] images = new int[]{
+                R.drawable.beemo,
+                R.drawable.earl,
+                R.drawable.marceline,
+                R.drawable.ice_king,
+                R.drawable.jake,
+                R.drawable.finn,
+                R.drawable.princess_bubblegum,
+                R.drawable.gunter};
+        private int[] ImageIndex = new int[NUM_IMAGES];
+
+        //private Chronometer timer;
+        private boolean firstPairRevealed;
+        private boolean[] imagePermanentlyRevealed = new boolean[NUM_IMAGES];
+        private int lastIndexClicked;
+        private static final int NONE = -1;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_memory, container, false);
+
+            // Get a reference to each button, store in our buttons array
+            for(int i = 0; i < NUM_IMAGES; i++){
+                images_list[i] = (ImageView) rootView.findViewById(getImageId("imageView" + i)); //agafar imatge per id
+            }
+
+            // Each image behaves the same; on click, call showPicture with a reference to the clicked button
+            for(int i = 0; i < images_list.length; i++){
+                final int index = i;
+                images_list[i].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onImageClicked(index, images_list[index]);
+                    }
+                });
+            }
+
+            //timer = (Chronometer) rootView.findViewById(R.id.timer);
+
+            busy = false;
+            lastIndexClicked = NONE;
+            resetImages();
+
+            return rootView;
+        }
+
+        /** A handy (but not recommended) way to retrieve a resource id by name in code. */
+        private int getImageId(final String imageIdName){
+            final int imageId = getResources().getIdentifier(imageIdName, "id", getActivity().getPackageName());
+            if(0 == imageId){
+                Log.e(LOG_TAG, "Could not find button with id " + imageIdName);
+                throw new RuntimeException("Cannot find necessary button..");
+            }
+            return imageId;
+        }
+
+        /** Reset the image of each button to the default back. */
+        private void resetImages(){
+            for(int i = 0; i < imagePermanentlyRevealed.length; i++){
+                imagePermanentlyRevealed[i] = false;
+            }
+            firstPairRevealed = false;
+
+            for(ImageView i : images_list){
+                i.setImageResource(R.drawable.adventuretime_logo);
+            }
+
+            randomizeImages();
+        }
+
+        private void randomizeImages(){
+            // Randomize button images
+            Random random = new Random();
+            int[] count = new int[images.length];
+            for(int i = 0; i < images_list.length; i++){
+                // Ensure max 2 of any given image are chosen
+                // Dirty solution, may not scale nicely depending on number of buttons / images
+                int candidateImageIndex = random.nextInt(images.length);
+                while(count[candidateImageIndex] >= 2){
+                    candidateImageIndex = random.nextInt(images.length);
+                }
+
+                count[candidateImageIndex]++;
+                ImageIndex[i] = candidateImageIndex;
+            }
+        }
+
+        private void hideUnrevealedImages(){
+            for(int i = 0; i < images_list.length; i++){
+                if(imagePermanentlyRevealed[i]){
+                    images_list[i].setImageResource(images[ImageIndex[i]]);
+                } else {
+                    images_list[i].setImageResource(R.drawable.adventuretime_logo);
+                }
+            }
+        }
+
+        private void onImageClicked(int indexClicked, ImageView i){
+            if(busy) return;
+            if(imagePermanentlyRevealed[indexClicked]) return; // Nothing to do here..
+
+            // Set the image of the button to its corresponding picture.
+            i.setImageResource(images[ImageIndex[indexClicked]]);
+            if(NONE == lastIndexClicked){
+                // This is the first button clicked.
+                lastIndexClicked = indexClicked;
+
+            } else if(lastIndexClicked != indexClicked){
+                // A unique second button was clicked!
+                if(!firstPairRevealed)
+                    firstPairRevealed = true;
+
+                if(match(lastIndexClicked, indexClicked)){
+                    imagePermanentlyRevealed[lastIndexClicked] = true;
+                    imagePermanentlyRevealed[indexClicked] = true;
+                    lastIndexClicked = NONE;
+
+                    checkVictoryState();
+                } else {
+                    lastIndexClicked = NONE;
+
+                    // Do not let the user perform clicks while we are waiting
+                    busy = true;
+
+                    // Call the hideUnrevealedButtons method after a delay
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            hideUnrevealedImages();
+                            busy = false;
+                        }
+                    }, 2000); //2 segons que han d'estar girades
+                }
+            }
+        }
+
+        /** @return true if buttons at given indices have an identical picture. */
+        private boolean match(int firstImageIndex, int secondImageIndex){
+            return ImageIndex[firstImageIndex] == ImageIndex[secondImageIndex];
+        }
+
+        /** Checks if the player has won the game. If so, resets the game.*/
+        private void checkVictoryState(){
+            for(boolean revealed : imagePermanentlyRevealed){
+                if(!revealed) return; // if any button has not been revealed, the game is on!
+            }
+
+            //TODO Congratulate the player with an alert dialog
+            Toast.makeText(getActivity(), "Congratulations! You win!", Toast.LENGTH_SHORT).show();
+
+            // Reset the game after a short delay
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    resetImages();
+                }
+            }, 1400);
+        }
     }
 }
--- UserProfile.java
+++ UserProfile.java
@@ -1,11 +1,24 @@
 package com.example.marlen.appjedi;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +29,9 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.List;
+
 public class UserProfile extends AppCompatActivity implements View.OnClickListener {
 
     TextView userName, name, points, number_points, address, adrs, notification, notif, pass;
@@ -24,6 +40,9 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
     ImageView profPic;
     DbHelper baseDades;
     LinearLayout layout;
+    List<Address> l;
+    LocationManager lm;
+    LocationListener lis;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,13 +67,13 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
         adrs = (TextView) findViewById(R.id.address_txt);
         notification = (TextView) findViewById(R.id.notif);
         notif = (TextView) findViewById(R.id.notif_txt);
-        pass = (TextView)findViewById(R.id.lePass);
-        password = (EditText)findViewById(R.id.lePassword);
-        ok = (Button)findViewById(R.id.ok);
+        pass = (TextView) findViewById(R.id.lePass);
+        password = (EditText) findViewById(R.id.lePassword);
+        ok = (Button) findViewById(R.id.ok);
         ok.setOnClickListener(this);
         profPic = (ImageView) findViewById(R.id.profilePic);
         profPic.setOnClickListener(this);
-        layout = (LinearLayout)findViewById(R.id.pass_layout);
+        layout = (LinearLayout) findViewById(R.id.pass_layout);
         baseDades = new DbHelper(this);
 
         Bundle bundle = getIntent().getExtras();
@@ -63,6 +82,65 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
         number_points.setText(points.toString());
         notif.setText(bundle.getString("notif"));
 
+        //GPS
+        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        lis = new LocationListener() {
+
+            @Override
+            public void onStatusChanged(String provider, int status,
+                                        Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+
+            @Override
+            public void onLocationChanged(Location location) {
+                Geocoder gc = new Geocoder(getApplicationContext());
+                try {
+                    l = gc.getFromLocation(location.getLatitude(),
+                            location.getLongitude(), 5);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                for (int i = 0; i < l.size(); ++i) {
+                    Log.v("LOG", l.get(i).getAddressLine(0).toString());
+                    if (i == 0) adrs.setText("");
+                    adrs.setText(adrs.getText() + "\n" + l.get(i).getAddressLine(0).toString());
+                }
+                Log.v("LOG", ((Double) location.getLatitude()).toString());
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, lis);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, lis);
+    }
+
+    @Override
+    protected void onPause() {
+        lm.removeGpsStatusListener((GpsStatus.Listener) lis);
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -72,11 +150,11 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
                 layout.setVisibility(View.VISIBLE);
                 return true;
             case R.id.log_out:
-                SharedPreferences culo = getSharedPreferences("culo",MODE_PRIVATE);
+                SharedPreferences culo = getSharedPreferences("culo", MODE_PRIVATE);
                 SharedPreferences.Editor editor = culo.edit();
                 editor.putString("userName", null); //buido el sharepreferences
                 editor.apply();
-                Intent intent = new Intent(getApplicationContext(),Login.class);
+                Intent intent = new Intent(getApplicationContext(), Login.class);
                 startActivity(intent);
                 finish(); //para q cuando tire atrás no vuelva al user_profile
                 return true;
@@ -89,11 +167,16 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.profilePic:
+                Intent pickAnImage = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickAnImage.setType("image/*");
 
+                startActivityForResult(pickAnImage, 2);
                 break;
             case R.id.ok:
                 String pass = password.getText().toString();
-                baseDades.updatePassword(userName.getText().toString(),pass);
+                baseDades.updatePassword(userName.getText().toString(), pass);
                 //Cursor c = baseDades.getUser(name.getText().toString());
                 //String polla = c.getString(c.getColumnIndex(baseDades.CN_PASS));
                 //Toast.makeText(getApplicationContext(), polla , Toast.LENGTH_SHORT).show();
@@ -101,5 +184,24 @@ public class UserProfile extends AppCompatActivity implements View.OnClickListen
                 break;
         }
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Como en este caso los 3 intents hacen lo mismo, si el estado es correcto recogemos el resultado
+        //Aún así comprobamos los request code. Hay que tener total control de lo que hace nuestra app.
+        if (resultCode == RESULT_OK) {
+            data.getData();
+            Uri selectedImage = data.getData();
+            Log.v("PICK", "Selected image uri" + selectedImage);
+            try {
+                profPic.setImageBitmap(MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.v("Result", "Something happened");
+        }
     }
 }
